93f03a3d0300553a3aeb181faaa66d003860cec3
carlos.tangarife <cjt860826@gmail.com>
2021-12-08 10:41:49 -0500

    the EnumHelper enum is added to convert an enum into a key-value list

---
+++ src/Mecalux.OrderingWords.Infrastructure/Helpers/EnumHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mecalux.OrderingWords.Infrastructure.Helpers
+{
+    public static class EnumHelper
+    {
+        public static List<KeyValuePair<string, int>> GetEnumList<T>()
+        {
+            var keyValuePairs = new List<KeyValuePair<string, int>>();
+            foreach (var e in Enum.GetValues(typeof(T)))
+            {
+                keyValuePairs.Add(new KeyValuePair<string, int>(e.ToString(), (int)e));
+            }
+            return keyValuePairs;
+        }
+    }
+}
