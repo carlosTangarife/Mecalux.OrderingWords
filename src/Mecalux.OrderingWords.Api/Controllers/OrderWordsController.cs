cc4c0c0a10db70671fa0fa271667b7fa71d73fa5
carlos.tangarife <cjt860826@gmail.com>
2021-12-08 10:54:44 -0500

    the OrderWordsController controller and the GetOrderOptionsy endpoint are created, additionally the IOrderOptionsRepository is injected to expose the ordering options.

---
+++ src/Mecalux.OrderingWords.Api/Controllers/OrderWordsController.cs
@@ -0,0 +1,33 @@
+﻿using Mecalux.OrderingWords.Application.Contracts.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace Mecalux.OrderingWords.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrderWordsController : ControllerBase
+    {
+
+        private readonly ILogger<OrderWordsController> _logger;
+
+        private readonly IOrderOptionsRepository _orderOptionsRepository;
+
+        public OrderWordsController(ILogger<OrderWordsController> logger, IOrderOptionsRepository orderOptions)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _orderOptionsRepository = orderOptions ?? throw new ArgumentNullException(nameof(orderOptions));
+        }
+
+        [HttpGet("GetOrderOptions")]
+        [ProducesResponseType(typeof(ICollection<KeyValuePair<string, int>>), (int)HttpStatusCode.OK)]
+        public IActionResult GetOrderOptions()
+        {
+            _logger.LogInformation("=>>>>>>>>>>>> Geting Order Options");
+            return Ok(_orderOptionsRepository.GetOrderOptions());
+        }
+    }
+}
