50271bc5042638900a8177c4aa4c0d68f52a25fb
carlos.tangarife <cjt860826@gmail.com>
2021-12-08 10:46:10 -0500

    OrderOptionsRepository is added, this will be in charge of exposing the data that the api needs to deliver.

---
+++ src/Mecalux.OrderingWords.Infrastructure/Repository/OrderOptionsRepository.cs
@@ -0,0 +1,15 @@
+﻿using Mecalux.OrderingWords.Application.Contracts.Repository;
+using Mecalux.OrderingWords.Applications.Enums;
+using Mecalux.OrderingWords.Infrastructure.Helpers;
+using System.Collections.Generic;
+
+namespace Mecalux.OrderingWords.Infrastructure.Repository
+{
+    public class OrderOptionsRepository : IOrderOptionsRepository
+    {
+        public ICollection<KeyValuePair<string, int>> GetOrderOptions()
+        {
+            return EnumHelper.GetEnumList<OrderOptions>();
+        }
+    }
+}
