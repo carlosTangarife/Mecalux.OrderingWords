5587b85a747ecb2ea6c135fc3c643b67598ac201
carlos.tangarife <cjt860826@gmail.com>
2021-12-08 11:13:52 -0500

    The IOrderWordsService interface and the OrderWordsService service are added, in which the necessary logic will be found to calculate the statistics of the text to be analyzed

---
+++ src/Mecalux.OrderingWords.Application/Contracts/Service/IOrderWordsService.cs
@@ -0,0 +1,11 @@
+﻿using Mecalux.OrderingWords.Domain.Entities;
+
+namespace Mecalux.OrderingWords.Application.Contracts.Service
+{
+    public interface IOrderWordsService
+    {
+        TextStatistics GetStatic(string textToAnalize);
+
+        int CountWordsByHyphens(string textToAnalize);
+    }
+}
---
+++ src/Mecalux.OrderingWords.Application/Services/OrderWordsService.cs
@@ -0,0 +1,29 @@
+﻿using Mecalux.OrderingWords.Application.Contracts.Service;
+using Mecalux.OrderingWords.Domain.Entities;
+using System.Text.RegularExpressions;
+
+namespace Mecalux.OrderingWords.Application.Services
+{
+    public class OrderWordsService : IOrderWordsService
+    {
+        public TextStatistics GetStatic(string textToAnalize)
+        {
+            var textSplitedBySpace = textToAnalize.Split(" ");
+
+            return new TextStatistics
+            {
+                HyphensQuantity = CountWordsByHyphens(textToAnalize),
+                WordQuantity = textSplitedBySpace.Length,
+                SpacesQuantity = textSplitedBySpace.Length - 1
+            };
+        }
+
+        public int CountWordsByHyphens(string textToAnalize)
+        {
+            string pattern = "[-]";
+            Regex regex = new(pattern);
+            MatchCollection matchedAuthors = regex.Matches(textToAnalize);
+            return matchedAuthors.Count;
+        }
+    }
+}
