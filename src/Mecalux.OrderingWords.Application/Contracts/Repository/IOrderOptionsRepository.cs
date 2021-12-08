7cb9c82099e6e53d9a239c09f5b75799e3ef1760
carlos.tangarife <cjt860826@gmail.com>
2021-12-08 10:38:23 -0500

    the IOrderOptionsRepository interface is created

---
+++ src/Mecalux.OrderingWords.Application/Contracts/Repository/IOrderOptionsRepository.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Mecalux.OrderingWords.Application.Contracts.Repository
+{
+    public interface IOrderOptionsRepository
+    {
+        ICollection<KeyValuePair<string, int>> GetOrderOptions();
+    }
+}
