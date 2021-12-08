641fc33843e4c69dd2e8f934732b196818a9d6bd
carlos.tangarife <cjt860826@gmail.com>
2021-12-08 11:08:25 -0500

    TextStatistics entity is added

---
+++ src/Mecalux.OrderingWords.Domain/Entities/TextStatistics.cs
@@ -0,0 +1,16 @@
+﻿namespace Mecalux.OrderingWords.Domain.Entities
+{
+    public class TextStatistics
+    {
+        public int HyphensQuantity { get; set; }
+        public int WordQuantity { get; set; }
+        public int SpacesQuantity { get; set; }
+
+        public TextStatistics()
+        {
+            HyphensQuantity = 0;
+            WordQuantity = 0;
+            SpacesQuantity = 0;
+        }
+    }
+}
