16a044e066dd83198542be5975a74ead511a282d
carlos.tangarife <cjt860826@gmail.com>
2021-12-08 11:16:11 -0500

    the dependency injector of the Mecalux.OrderingWords.Applications layer was created and registered in the startup of the Mecalux.OrderingWords.Api layer

--- src/Mecalux.OrderingWords.Api/Startup.cs
+++ src/Mecalux.OrderingWords.Api/Startup.cs
@@ -1,17 +1,11 @@
+using Mecalux.OrderingWords.Application;
 using Mecalux.OrderingWords.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Mecalux.OrderingWords.Api
 {
@@ -28,6 +22,7 @@ namespace Mecalux.OrderingWords.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddInfrastructureServicesRegistration();
+            services.AddApplicationServices();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
---
+++ src/Mecalux.OrderingWords.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,15 @@
+﻿using Mecalux.OrderingWords.Application.Contracts.Service;
+using Microsoft.Extensions.DependencyInjection;
+using Mecalux.OrderingWords.Application.Services;
+
+namespace Mecalux.OrderingWords.Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IOrderWordsService, OrderWordsService>();
+            return services;
+        }
+    }
+}
