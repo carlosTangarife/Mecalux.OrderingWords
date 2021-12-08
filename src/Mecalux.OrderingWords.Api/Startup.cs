c106a2ed74f04edeee3f78b684c10dc56caded5c
carlos.tangarife <cjt860826@gmail.com>
2021-12-08 10:51:44 -0500

    the dependency injector of the Mecalux.OrderingWords.Infrastructure layer is registered in the startup of the Mecalux.OrderingWords.Api layer

--- src/Mecalux.OrderingWords.Api/Startup.cs
+++ src/Mecalux.OrderingWords.Api/Startup.cs
@@ -1,3 +1,4 @@
+using Mecalux.OrderingWords.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,7 +27,7 @@ namespace Mecalux.OrderingWords.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddInfrastructureServicesRegistration();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
