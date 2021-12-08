15074ce4afc878f4bf257039afa434da696243b4
carlos.tangarife <cjt860826@gmail.com>
2021-12-08 11:20:36 -0500

    GetStatic endpoint is added to the OrderWordsController controller, this endpoint will expose the number of spaces, words and hyphens in a text.

--- src/Mecalux.OrderingWords.Api/Controllers/OrderWordsController.cs
+++ src/Mecalux.OrderingWords.Api/Controllers/OrderWordsController.cs
@@ -1,4 +1,6 @@
 ﻿using Mecalux.OrderingWords.Application.Contracts.Repository;
+using Mecalux.OrderingWords.Application.Contracts.Service;
+using Mecalux.OrderingWords.Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -16,10 +18,14 @@ namespace Mecalux.OrderingWords.Api.Controllers
 
         private readonly IOrderOptionsRepository _orderOptionsRepository;
 
-        public OrderWordsController(ILogger<OrderWordsController> logger, IOrderOptionsRepository orderOptions)
+        private readonly IOrderWordsService _orderingWordsService;
+
+        public OrderWordsController(ILogger<OrderWordsController> logger, IOrderOptionsRepository orderOptions, IOrderWordsService orderingWordsService)
         {
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _orderingWordsService = orderingWordsService ?? throw new ArgumentNullException(nameof(orderingWordsService));
             _orderOptionsRepository = orderOptions ?? throw new ArgumentNullException(nameof(orderOptions));
+            _orderingWordsService = orderingWordsService ?? throw new ArgumentNullException(nameof(orderOptions));
         }
 
         [HttpGet("GetOrderOptions")]
@@ -28,6 +34,20 @@ namespace Mecalux.OrderingWords.Api.Controllers
         {
             _logger.LogInformation("=>>>>>>>>>>>> Geting Order Options");
             return Ok(_orderOptionsRepository.GetOrderOptions());
+        }
+
+        [HttpGet("GetStatic")]
+        [ProducesResponseType(typeof(TextStatistics), (int)HttpStatusCode.OK)]
+        public IActionResult GetStatic(string textToAnalize)
+        {
+            _logger.LogInformation("=>>>>>>>>>>>> Geting Statics");
+            if (string.IsNullOrEmpty(textToAnalize))
+            {
+                return Ok(new TextStatistics { });
+            }
+
+            var result = _orderingWordsService.GetStatic(textToAnalize.Trim());
+            return Ok(result);
         }
     }
 }
