dc59d8d1e7b819b5d62e05caaa319bd5952c8be3
carlos.tangarife <cjt860826@gmail.com>
2021-12-08 10:50:10 -0500

    a class is created to register the dependency injection of the Mecalux.OrderingWords.Infrastructure layer

---
+++ src/Mecalux.OrderingWords.Infrastructure/InfrastructureServicesRegistration.cs
@@ -0,0 +1,16 @@
+﻿using Mecalux.OrderingWords.Application.Contracts.Repository;
+using Mecalux.OrderingWords.Infrastructure.Repository;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Mecalux.OrderingWords.Infrastructure
+{
+    public static class InfrastructureServicesRegistration
+    {
+        public static IServiceCollection AddInfrastructureServicesRegistration(this IServiceCollection services)
+        {
+            services.AddScoped<IOrderOptionsRepository, OrderOptionsRepository>();
+
+            return services;
+        }
+    }
+}
